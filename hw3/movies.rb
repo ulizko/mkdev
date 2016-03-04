04a13e8a91f9a54d8aa3a8e4c8012391d9dfd467
ulizko <shurikovich@bigmir.net>
2016-03-04 06:57:09 +0000

    Methods slice_movies, present_rating_as_stars

--- hw3/movies.rb
+++ hw3/movies.rb
@@ -28,26 +28,67 @@ def search(arr, field, str)
   filtred = arr.select do |key, value|
     value[field].downcase.include? str.downcase
   end
-  filtred.values
+  filtred
 end
 
 def sort_movies(arr, field, reversed = false)
   sorted = arr.sort_by do |key, value|
-    value[field].to_f.zero? ? value[field] : value[field].to_f
+    case field
+    when :title, :country, :release, :genres, :starring
+      value[field]
+    when :year, :time
+      value[field].to_f
+    when :director
+      value[field].split(" ").last
+    end
   end
   reversed ? sorted.reverse : sorted
 end
 
 def present_rating_as_stars(arr)
-  arr.map do |value|
+  arr.map do |key, value|
     rating = ((value[:rating].to_f.ceil - value[:rating].to_f)*10).to_i
     value[:rating] = "".ljust(rating, "*")
   end
   arr
 end
 
-s = present_rating_as_stars(search(movies, :title, "Time"))
-s.each do |value|
+def slice_movies(arr, *option)
+  arr.values.map! do |v|
+     v.select {|k, _| option.include? k}
+  end
+end
+
+
+
+=begin
+stars = present_rating_as_stars(search(movies, :title, "Time"))
+stars.each do |_, value|
   puts "#{value[:title]} #{value[:rating]}"
 end
-#puts sort_movies(movies, :country).first(5)
+
+the_longest_movies = sort_movies(movies, :time, true).first(5)
+the_longest_movies.each do |_, value| 
+  puts "#{value[:title]} #{value[:time]}"
+end
+
+comedy = sort_movies(search(movies, :genres, "Comedy"), :release)
+comedy.each do |_, value|
+  puts "#{value[:title]} #{value[:release]}"
+end
+
+directors = sort_movies(movies, :director).uniq {|_, v| v[:director]}
+directors.each do |_, value|
+  puts value[:director]
+end
+
+country_not_usa = movies.count {|_, v| v[:country] != "USA"}
+p country_not_usa
+
+
+group_by_directors = movies.values.group_by{|val| val[:director]}.each{|_, v| v.map!{|m| m[:title]}}
+
+p group_by_directors
+
+=end
+p slice_movies(movies, :title, :starring)
