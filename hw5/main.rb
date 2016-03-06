2f5cfaca30c911ae71d2152a0c3afa2d80e308b5
ulizko <shurikovich@bigmir.net>
2016-03-06 16:37:53 +0000

    Add sort method

--- hw5/main.rb
+++ hw5/main.rb
@@ -1,11 +1,7 @@
 require_relative 'movie.rb'
 require_relative 'movies_list.rb'
-puts "Hello. Enter file name"
-begin
+
+
   file_name = gets.chomp
-  movie_list = MoviesList.new(file_name)
-  rescue Exception
-  puts "File not found: #{file_name}"
   movie_list = MoviesList.new('movies.txt')
-  exit
-end
+
--- hw5/movies_list.rb
+++ hw5/movies_list.rb
@@ -1,3 +1,13 @@
 require_relative 'movie.rb'
 class MoviesList
+
+  FIELDS = [:url, :title, :year, :country, :release, :genres, :time, :rating, :director, :starring]
+  
+  def initialize(file_name)
+    @movies_list = File.read(file_name).split("\n").map {|v| Movie.new(FIELDS.zip(v.split("|")).to_h)}
+  end
+  
+  def sort_by_field(field)
+    @movies_list.sort_by{|v| v.send(field)}
+  end
 end
