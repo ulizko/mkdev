5fc07962a043bbc9c49b8e1a05ec599d860479b8
ulizko <shurikovich@bigmir.net>
2016-03-06 16:45:32 +0000

    Sort finished

--- hw5/movies_list.rb
+++ hw5/movies_list.rb
@@ -7,7 +7,17 @@ class MoviesList
     @movies_list = File.read(file_name).split("\n").map {|v| Movie.new(FIELDS.zip(v.split("|")).to_h)}
   end
   
-  def sort_by_field(field)
-    @movies_list.sort_by{|v| v.send(field)}
+  def sort_by_field(field, reversed = false)
+    sorted = @movies_list.sort_by do |v| 
+      case field
+      when :title, :country, :release, :genres, :starring
+        v.send(field)
+      when :year, :time
+        v.send(field).to_f
+      when :director
+        v.send(field).split(" ").last
+      end
+    end
+    reversed ? sorted.reverse : sorted
   end
 end
