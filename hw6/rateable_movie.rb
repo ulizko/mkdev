636028f06e9c60f2aea1562edc86d7168bde8d48
ulizko <shurikovich@bigmir.net>
2016-03-14 20:26:00 +0000

    Replace rate, unwatched ...

--- hw6/rateable_movie.rb
+++ hw6/rateable_movie.rb
@@ -8,16 +8,4 @@ class RateableMovie < Movie
     @user_rate, @time_watch = user_rate, time_watch
   end
   
-  def rate(user_rate, time_rate = Time.now)
-    @user_rate = user_rate
-    @time_rate = time_rate if time_rate.is_a? Time
-  end
-  
-  def unwatched?
-    self.time_watch.nil?
-  end
-  
-  def days_after_watching
-    (Time.now - self.time_watch) / (60 * 60 * 24)
-  end
 end
--- hw6/recommendation.rb
+++ hw6/recommendation.rb
@@ -8,4 +8,17 @@ module Recommendation
     @movies_list.reject(&:unwatched?).sort_by{ |mov| mov.user_rate * mov.days_after_watching * 0.01 * rand }.last(5)
   end
   
+  def rate(user_rate, time_rate = Time.now)
+    @user_rate = user_rate
+    @time_rate = time_rate if time_rate.is_a? Time
+  end
+  
+  def unwatched?
+    self.time_watch.nil?
+  end
+  
+  def days_after_watching
+    (Time.now - self.time_watch) / (60 * 60 * 24)
+  end
+  
 end
