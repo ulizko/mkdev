9d0e6502b9de94c4b19ca4ad2f92248983021d6f
ulizko <shurikovich@bigmir.net>
2016-04-09 14:24:17 +0000

    Add spec for movie_db

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'mechanize', '2.7.4'
+gem 'themoviedb'
 
 group :test do
   gem 'rspec'
---
+++ spec/movie_db_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe "MovieDB" do
+  context "should return" do
+    it "title"
+    it "year"
+    it "country"
+    it "release date"
+    it "genres"
+    it "duration"
+    it "rating"
+    it "director"
+    it "actors"
+  end
+end
